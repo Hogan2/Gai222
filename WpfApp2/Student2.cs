2185232c69c76b149b1838c06a382f47705756bc
Hogan2 <loggg2333@gmail.com>
2018-06-14 14:17:24 +0800

    branchtest

--- WpfApp2/Student2.cs
+++ WpfApp2/Student2.cs
@@ -29,6 +29,10 @@ namespace WpfApp2
         public static readonly DependencyProperty Name3Property =
             DependencyProperty.Register("Name3", typeof(string), typeof(Student2), new PropertyMetadata(""));
 
+        public void Branchtest()
+        {
+
+        }
 
     }
 }
